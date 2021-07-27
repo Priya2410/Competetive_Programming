95b0075d0830d7026e263d4057100b89a887b218
Priya Mohata <56394628+Priya2410@users.noreply.github.com>
2021-07-27 09:23:23 +0530

    Update BalanceBracketsSequel.cpp

--- BinarySearch.com/BalanceBracketsSequel.cpp
+++ BinarySearch.com/BalanceBracketsSequel.cpp
@@ -15,3 +15,26 @@ bool solve(string s) {
     else
         return false;
 }
+
+// OR
+
+// bool solve(string s) {
+//     stack<char> s1;
+//     for (int i = 0; i < s.length(); i++) {
+//         if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+//         s1.push(s[i]);
+//         else{
+//             if(s1.empty())
+//             return false;
+//             else if(s[i]==')' && s1.top()!='(')
+//             return false;
+//             else if(s[i]=='}' && s1.top()!='{')
+//             return false;
+//             else if(s[i]==']' && s1.top()!='[')
+//             return false;
+//             else
+//             s1.pop();
+//         }
+//     }
+//     return s1.empty();
+// }
